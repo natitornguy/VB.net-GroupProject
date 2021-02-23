fc3242204ffbc7ac117606e679e1b3304bc8e903
natitornguy <ka.natitorn_st@tni.ac.th>
2021-02-23 22:08:17 +0700

    Temporary Login system

--- VbProject/Login.vb
+++ VbProject/Login.vb
@@ -1,8 +1,14 @@
 ﻿Public Class Login
     Private Sub btn_login_Click(sender As Object, e As EventArgs) Handles btn_login.Click
         'Main_Menu(0) คือล็อกอินเป็นแอดมิน (1) คือ ผู้ใช้ธรรมดา
-        Dim main = New Main_Menu(1)
-        main.Show()
+        Dim user As Integer
+        If txt_username.Text = "admin" Then
+            user = 0
+        ElseIf txt_username.Text = "user" Then
+            user = 1
+        End If
+        Dim main = New Main_Menu(user)
+            main.Show()
         Me.Close()
     End Sub
 
