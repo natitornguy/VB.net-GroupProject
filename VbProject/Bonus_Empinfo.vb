82a40e9dd66b016b124ced1a7f48ff4c1382fcf0
chayapol <sr.thanakorn_st@tni.ac.th>
2021-02-24 16:57:28 +0700

    bonus complete

--- VbProject/Bonus_Empinfo.vb
+++ VbProject/Bonus_Empinfo.vb
@@ -10,13 +10,33 @@
         ' Add any initialization after the InitializeComponent() call.
 
     End Sub
-    Private Sub Bonus_Empinfo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    Private Sub loaddata()
         Dim sqltext = "select emp_id,CONCAT(emp_fname,' ',emp_lname) as name,emp_salary,emp_bonus,leave_count,dep_name,grade
                         from employees e
                         JOIN departments d
                         ON e.DEP_ID = d.DEP_ID"
         Dim result = ConnectDB.QueryAdapter(sqltext)
         dtg_empinfo.DataSource = result.Tables("data")
+    End Sub
+
+    Private Sub searchdata(name)
+        If name <> "" Then
+            Dim sqltext = "select emp_id,CONCAT(emp_fname,' ',emp_lname) as name,emp_salary,emp_bonus,leave_count,dep_name,grade
+                        from employees e
+                        JOIN departments d
+                        ON e.DEP_ID = d.DEP_ID
+                        where emp_fname like '%" & name & "%' or
+                        emp_lname like '%" & name & "%'"
+            Dim result = ConnectDB.QueryAdapter(sqltext)
+            dtg_empinfo.DataSource = result.Tables("data")
+        Else
+            loaddata()
+        End If
+
+    End Sub
+    Private Sub Bonus_Empinfo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loaddata()
     End Sub
 
     Private Sub btn_evaluate_Click(sender As Object, e As EventArgs) Handles btn_evaluate.Click
@@ -30,5 +50,10 @@
         Dim fmain As New Main_Menu(status)
         fmain.Show()
         Me.Close()
+    End Sub
+
+    Private Sub btn_search_Click(sender As Object, e As EventArgs) Handles btn_search.Click
+        Dim name = txt_fname_search.Text
+        searchdata(name)
     End Sub
 End Class
--- VbProject/Cal_Bonus.vb
+++ VbProject/Cal_Bonus.vb
@@ -10,13 +10,22 @@
         ' Add any initialization after the InitializeComponent() call.
 
     End Sub
-    Private Sub btn_back_Click(sender As Object, e As EventArgs) Handles btn_back.Click
-        Dim fbonus_info As New Bonus_Empinfo(status)
-        fbonus_info.Show()
-        Me.Close()
+
+    Private Sub loaddata()
+        Dim sqltext = "select emp_fname,emp_lname,emp_salary,d.DEP_NAME,grade 
+                        from employees e
+                        JOIN departments d
+                        ON e.DEP_ID = d.DEP_ID
+                        where emp_id = " & id
+        Dim result As ArrayList = ConnectDB.QueryReader(sqltext, 5)
+        txt_fname.Text = result(0)
+        txt_lname.Text = result(1)
+        txt_salary.Text = result(2)
+        txt_department.Text = result(3)
+        cbo_grade.SelectedIndex = If(IsDBNull(result(4)), -1, result(4) - 1)
     End Sub
 
-    Private Sub btn_submit_Click(sender As Object, e As EventArgs) Handles btn_submit.Click
+    Private Sub evaluate_emp()
         Dim gradeid = cbo_grade.SelectedIndex + 1
         Dim sqltext = "select mas_grade_bonus from mas_grade where mas_grade_id = " & gradeid
         Dim gradenum = ConnectDB.QueryGetone(sqltext)
@@ -35,22 +44,25 @@
             fbonus_info.Show()
             Me.Close()
         End If
+    End Sub
+
+    Private Sub btn_back_Click(sender As Object, e As EventArgs) Handles btn_back.Click
+        Dim fbonus_info As New Bonus_Empinfo(status)
+        fbonus_info.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btn_submit_Click(sender As Object, e As EventArgs) Handles btn_submit.Click
+        Dim index = cbo_grade.SelectedIndex
+        If index <> -1 Then
+            evaluate_emp()
+        Else
+            MessageBox.Show("กรุณาประเมินพนักงานด้วย !!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
 
     End Sub
 
     Private Sub Cal_Bonus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim sqltext = "select emp_fname,emp_lname,emp_salary,d.DEP_NAME,grade 
-                        from employees e
-                        JOIN departments d
-                        ON e.DEP_ID = d.DEP_ID
-                        where emp_id = " & id
-        Dim result As ArrayList = ConnectDB.QueryReader(sqltext, 5)
-        txt_fname.Text = result(0)
-        txt_lname.Text = result(1)
-        txt_salary.Text = result(2)
-        txt_department.Text = result(3)
-        cbo_grade.SelectedIndex = If(IsDBNull(result(4)), -1, result(4) - 1)
-
-
+        loaddata()
     End Sub
 End Class
